3388976b1b9bedc5c7be616e66b5a7950426c3d0
Bhardwaj Bhaskar <bhaskar0120@gmail.com>
2023-12-24 01:40:53 +0530

    Raw format basic working

--- converter.py
+++ converter.py
@@ -1,10 +1,22 @@
 
-def simple():
-    pass
+def raw(file,size=(40,30)):
+    from cv2 import VideoCapture, imshow, waitKey, resize
+    video = VideoCapture(file)
+    cols, rows = size
+    success, frame = video.read()
+    with open('out.bin','wb') as f:
+        f.write(rows.to_bytes(4,'little')+cols.to_bytes(4,'little'))
+        while success:
+            frame = resize(frame,size);
+            f.write(frame[:,:,0].tobytes())
+            success, frame = video.read()
 
-from sys import argv
+    
+
+
+
 compressionType = {
-        'simple' : simple
+        'raw' : raw
         }
 
 types = [
@@ -12,15 +24,16 @@ types = [
         ]
 
 def err_exit(errno):
-    print("USAGE: ./python3 converter.py [Type] \n\nAvailable Types")
+    print("USAGE: ./python3 converter.py [Type] [Video File] \n\nAvailable Types")
     for i,t,desc in types:
         print('{}. {} - {}.'.format(i,t,desc))
     exit(errno)
-        
+
 if __name__ == "__main__":
-    if len(argv) != 2:
+    from sys import argv
+    if len(argv) != 3:
         err_exit(1)
     if argv[1] not in compressionType:
         err_exit(1)
-    compressionType[argv[1]]()
+    compressionType[argv[1]](argv[2])
 
--- main.c
+++ main.c
@@ -1,23 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 
 void clear(){
   printf("\x1b[2J");
 }
 
+void raw(FILE* file, int rows, int cols){
+  size_t readSoFar = 0;
+  char* buffer = (char*)malloc(rows*cols+rows);
+  char* data = (char*)malloc(rows*cols);
+  for(int i = 0; i < rows; ++i){
+    buffer[i*(cols+1) + cols] = '\n';
+  }
+  while(readSoFar < 6572){
+    clear();
+    size_t alreadyRead = 0;
+    while(alreadyRead < rows*cols){
+      alreadyRead += fread(data+alreadyRead,1,min(4096,rows*cols-alreadyRead),file);
+    }
+
+    for(int i = 0; i < rows; ++i){
+      for(int j = 0; j < cols; ++j){
+        buffer[i*(cols+1)+j] = (data[i*cols+j] > 200u)?'@':' ';
+      }
+    }
+    printf("%s",buffer);
+    readSoFar++; 
+    Sleep(1000.0/30.0);
+  }
+  
+  free(buffer);
+  free(data);
+}
+
+
+
 int main(int argc, char **argv){
-  clear();
-  if(argc != 3){
+  if(argc != 2){
     return 1;
+    //TODO Write argError
   }
-  int rows = strtol(argv[1],NULL,10);
+
+  char* vidFile = argv[1];
+
+  FILE *file = fopen(vidFile,"rb");
   if(errno){
     return 1;
+    //TODO Write fileNotFound
   }
-  int cols = strtol(argv[2],NULL,10);
+  int rows, cols;
+  fread(&rows, 4,1,file);
+  fread(&cols, 4,1,file);
+  
+
+  if(rows*cols >= 1000000){
+    return 1;
+    // TODO Write memSizeExceeded
+  }
+  raw(file,rows,cols);
+
+
+  fclose(file);
   if(errno){
     return 1;
+    //TODO Write fileCloseError
   }
+
+
+
   return 0;
 }
 
